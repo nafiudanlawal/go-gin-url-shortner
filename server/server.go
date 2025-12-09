beb4b4817fecbc6b517ae0e423fa50f6d3c334c4
Nafiu Lawal <nafiudanlawal@gmail.com>
2025-12-09 20:14:16 +0100

    frontend

--- server/server.go
+++ server/server.go
@@ -2,25 +2,24 @@ package server
 
 import (
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"net/http"
 	"time"
 	"url-shortening-service/routes"
 	"url-shortening-service/utils"
-
-	"github.com/gin-gonic/gin"
 )
 
-func Run(){
-	Router := gin.New()
-	Router.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+func Run() {
+	router := gin.New()
+	router.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
 		// custom format
 		return fmt.Sprintf("%s | %10s |%s%-5s%s| %s%5d%s | %8s |  %18s | %s | %s | %s\n",
 			param.TimeStamp.Format(time.RFC3339),
 			param.ClientIP,
 			param.MethodColor(),
 			param.Method,
-      param.ResetColor(),
-      param.StatusCodeColor(),
+			param.ResetColor(),
+			param.StatusCodeColor(),
 			param.StatusCode,
 			param.ResetColor(),
 			param.Latency,
@@ -30,17 +29,29 @@ func Run(){
 			param.ErrorMessage,
 		)
 	}))
-	Router.Use(gin.Recovery())
+	router.Use(gin.Recovery())
+	router.LoadHTMLFiles("static/index.tmpl")
+	router.Static("/static", "./static")
 
-	routes.AddRoutes(&Router.RouterGroup)
+	routes.AddRoutes(&router.RouterGroup)
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{})
+	})
+	router.GET("/:code", func(c *gin.Context) {
+		code := c.Params.ByName("code")
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"code": code,
+		})
+	})
 
 	s := &http.Server{
-    Addr:           fmt.Sprintf("127.0.0.1:%s", utils.GetEnvVars()["PORT"]),
-    Handler:        Router,
-    ReadTimeout:    10 * time.Second,
-    WriteTimeout:   10 * time.Second,
-    MaxHeaderBytes: 1 << 20,
-  }
-  s.ListenAndServe()
-  
+		Addr:           fmt.Sprintf("127.0.0.1:%s", utils.GetEnvVars()["PORT"]),
+		Handler:        router,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	s.ListenAndServe()
+
 }
